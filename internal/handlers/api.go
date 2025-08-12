1dd4d527283120ec7ec667b4a08d8a5fc9e31730
Alex <andringax@gmail.com>
2025-08-12 16:58:31 -0500

    Fixed name import

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"github.com/alexand-x/store_path/internal/middleware"
+	"github.com/andringa-x/store_path/internal/middleware"
 	"github.com/go-chi/chi"
 	chimiddle "github.com/go-chi/chi/middleware"
 )
